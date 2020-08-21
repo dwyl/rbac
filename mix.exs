89e14f943ed357d03176143cf4099d4dcc865a72
nelsonic <nelson+github@dwyl.io>
2020-08-21 12:37:59 +0100

    add excoveralls to mix.* for #5

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Rbac.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Helper functions for Role Based Access Control (RBAC)",
+      package: package(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +30,18 @@ defmodule Rbac.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Check test coverage
+      {:excoveralls, "~> 0.13.1", only: :test}
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w(lib LICENSE mix.exs README.md),
+      name: "rbac",
+      licenses: ["GNU GPL v2.0"],
+      maintainers: ["dwyl"],
+      links: %{"GitHub" => "https://github.com/dwyl/rbac"}
     ]
   end
 end
