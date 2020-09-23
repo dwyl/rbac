9895e54bacd0a4c3ce522d38c67cca0fd53afb81
nelsonic <nelson+github@dwyl.io>
2020-09-23 17:14:08 +0100

    bump version to 0.5.2 to include list_approles/2 #12

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rbac.MixProject do
   def project do
     [
       app: :rbac,
-      version: "0.5.1",
+      version: "0.5.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
