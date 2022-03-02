27f428763bb6451d1be6643dcbb6c92d7de96379
SimonLab <simon.labondance@gmail.com>
2022-03-02 10:17:42 +0000

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rbac.MixProject do
   def project do
     [
       app: :rbac,
-      version: "0.7.0",
+      version: "0.7.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
