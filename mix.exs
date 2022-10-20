66b6ecae51c59d6e33fa4c84a6a0a76d6d278162
nelsonic <nelson+github@dwyl.com>
2022-10-20 13:02:22 +0100

    bump version from 1.0.2 to 1.0.3 https://github.com/dwyl/rbac/pull/25

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rbac.MixProject do
   def project do
     [
       app: :rbac,
-      version: "1.0.2",
+      version: "1.0.3",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
