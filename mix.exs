3ec9c026cc822186ba82c03ec265700ddc36e8da
nelsonic <nelson+github@dwyl.com>
2026-09-02 14:37:08 +0100

    mix deps.clean ssl_verify_fun

--- mix.exs
+++ mix.exs
@@ -10,13 +10,17 @@ defmodule Rbac.MixProject do
       deps: deps(),
       description: "Helper functions for Role Based Access Control (RBAC)",
       package: package(),
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
-      ]
+      test_coverage: [tool: ExCoveralls]
+    ]
+  end
+
+  def cli do
+    [preferred_envs: [
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.post": :test,
+      "coveralls.html": :test
+    ]
     ]
   end
 
